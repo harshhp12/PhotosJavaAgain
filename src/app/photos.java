092c2f953cb8ddf6dc78f0c02d60494535ced208
Rithvik <rra76@scarletmail.rutgers.edu>
2020-11-04 22:20:43 -0500

    package structure

---
+++ src/app/photos.java
@@ -0,0 +1,38 @@
+package app;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import controller.loginC;
+public class photos extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		FXMLLoader loader = new FXMLLoader();   
+		loader.setLocation(
+				getClass().getResource("/view/list.fxml"));
+		BorderPane root = (BorderPane)loader.load();
+
+		loginC loginController = loader.getController();
+		loginController.start(primaryStage);
+
+		Scene scene = new Scene(root, 800, 650);
+		primaryStage.setScene(scene);
+		primaryStage.show(); 
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+
+	}
+}
---
+++ src/controller/loginC.java
@@ -0,0 +1,27 @@
+package controller;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import app.photos;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class loginC {
+	public void start(Stage mainStage) {    
+
+		
+	}
+	
+}
