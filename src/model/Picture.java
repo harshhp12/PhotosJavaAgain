ce760854f1bfb67c5b7f3090fb879c46f6eae284
Rithvik <rra76@scarletmail.rutgers.edu>
2020-11-13 23:55:36 -0500

    Picture object class

--- src/model/Picture.java
+++ src/model/Picture.java
@@ -1,14 +1,43 @@
 package model;
 import java.io.Serializable;
 import java.util.Calendar;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Picture implements Serializable {
-
+	
+	private String caption;
+	private Calendar calendar;
+	private ByteStreamPhoto photo;
+	private List <Tag> tags;
+	
 	public Picture() {
-		//date
-		//tag
 		
+		caption="";
+		calendar = Calendar.getInstance();
+		calendar.set(Calendar.MILLISECOND, 0);
+		photo = new ByteStreamPhoto();
+		tags = new ArrayList<Tag>();
+	}
+	
+	//ex: location, New Brunswick
+	public void addTag(String name, String value) {
 		
 	}
+	
+	public void editTag() {
+		
+	}
+	
+	public void deleteTag() {
+		
+	}
+	
+
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+	
+	
 
 }
