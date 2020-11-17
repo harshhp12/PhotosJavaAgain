d2a35e16a146ae84e16bc676dea176e210ec94a9
Rithvik <rra76@scarletmail.rutgers.edu>
2020-11-17 18:07:12 -0500

    stockPhotos folder

---
+++ src/controller/albumC.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javafx.stage.Stage;
+
+public class albumC implements LogOff{
+	/**
+	 * 
+	 * @param app_stage
+	 */
+	public void start(Stage app_stage) {
+		
+	}
+}
--- src/model/Picture.java
+++ src/model/Picture.java
@@ -11,23 +11,28 @@ import java.util.ArrayList;
  */
 public class Picture implements Serializable {
 	
-	/**
-	 * 	 Generated serialVersionUID
-	 */
-	private static final long serialVersionUID = 1L;
+	private String photoPath;
 	private String caption;
 	private Calendar calendar;
 	private List <Tag> tags;
-	
-	public Picture() {
-		
-		caption="";
-		calendar = Calendar.getInstance();
-		calendar.set(Calendar.MILLISECOND, 0);
-		tags = new ArrayList<Tag>();
+	/**
+	 * Constructor one for picture
+	 * @param photoPath
+	 */
+	public Picture(String photoPath) {
+		this.photoPath = photoPath;
+		this.caption="";
+		this.calendar = Calendar.getInstance();
+		this.calendar.set(Calendar.MILLISECOND, 0);
+		this.tags = new ArrayList<Tag>();
 	}
 	
-	//ex: location, New Brunswick
+
+	/**
+	 * ex: location, New Brunswick
+	 * @param name
+	 * @param value
+	 */
 	public void addTag(String name, String value) {
 		
 	}
@@ -35,12 +40,22 @@ public class Picture implements Serializable {
 	public void editTag() {
 		
 	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getPhotoPath() {
+		return photoPath;
+	}
 	
 	public void deleteTag() {
 		
 	}
 	
-
+	/**
+	 * Set caption of the photo
+	 * @param caption
+	 */
 	public void setCaption(String caption) {
 		this.caption = caption;
 	}
