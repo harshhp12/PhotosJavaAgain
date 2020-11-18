847db7c3e7c99da22a684a2daf43202a45cc9515
Harsh Patel <harsh.patel12@rutgers.edu>
2020-11-17 22:41:33 -0500

    new search photos

--- src/controller/searchC.java
+++ src/controller/searchC.java
@@ -1,7 +1,16 @@
 package controller;
 
+import java.io.IOException;
+
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.ListUsers;
 
 public class searchC implements LogOff {
 	/**
@@ -13,6 +22,40 @@ public class searchC implements LogOff {
 	protected void handleLogout(ActionEvent event) throws ClassNotFoundException {
 	    logout(event);     
 	}
+	
+	/**
+	 * Safely quit
+	 * @param event
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
+	@FXML protected void handleQuit(ActionEvent event) throws ClassNotFoundException, IOException{
+		Platform.exit();
+
+	}
+	/**
+	 * Go back to user page
+	 * @param event
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
+	@FXML protected void handleBack(ActionEvent event) throws ClassNotFoundException, IOException{
+		Parent parent;
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/user.fxml"));
+		parent = (Parent) loader.load();
+		//searchC controller = loader.getController();
+		
+		//setup the scene
+		Scene scene = new Scene(parent);
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+		//call start method to setup showing the albums
+		//controller.start(stage);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+
 
 
 	
